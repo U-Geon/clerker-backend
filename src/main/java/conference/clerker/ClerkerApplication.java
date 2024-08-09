4009b9c523ae592fe1ce8cd7887b667d8adf0bea
U-Geon <sksmsfbrjs@naver.com>
2024-08-09 23:58:58 +0900

    first setting

---
+++ src/main/java/conference/clerker/ClerkerApplication.java
@@ -0,0 +1,13 @@
+package conference.clerker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClerkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClerkerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/conference/clerker/ClerkerApplicationTests.java
@@ -0,0 +1,13 @@
+package conference.clerker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClerkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

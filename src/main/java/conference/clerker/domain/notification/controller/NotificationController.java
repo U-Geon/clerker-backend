a399bcd8aae56c910f600db884b79b802736d6f8
U-Geon <sksmsfbrjs@naver.com>
2024-10-16 19:36:11 +0900

    Refactor: Swagger 설정 코드 수정 및 Controller API Response 양식 제거

--- src/main/java/conference/clerker/domain/notification/controller/NotificationController.java
+++ src/main/java/conference/clerker/domain/notification/controller/NotificationController.java
@@ -25,9 +25,6 @@ public class NotificationController {
 
     @GetMapping
     @Operation(summary = "알림 목록", description = "알림 예시> 0729 자이 회의 이(가) 09/27 ~ 09/28 까지 회의 이(가) 있습니다.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "성공", content = @Content(mediaType = "application/json")),
-    })
     public ResponseEntity<List<NotificationsResponseDTO>> findAll(
             @AuthenticationPrincipal OAuth2UserPrincipal principal) {
         return ResponseEntity.ok().body(notificationService.findAllByMemberId(principal.getMember().getId()));
@@ -35,9 +32,6 @@ public class NotificationController {
 
     @DeleteMapping("/{notificationID}")
     @Operation(summary = "알림 삭제")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "204", description = "성공", content = @Content(mediaType = "application/json")),
-    })
     public ResponseEntity<Void> delete(
             @Parameter(required = true, description = "프로젝트 Id" ,in = ParameterIn.PATH)
             @PathVariable("notificationID") Long notificationId) {
--- src/main/java/conference/clerker/domain/schedule/controller/ScheduleController.java
+++ src/main/java/conference/clerker/domain/schedule/controller/ScheduleController.java
@@ -40,11 +40,6 @@ public class ScheduleController {
 
     @PostMapping("/create/{projectID}")
     @Operation(summary = "스케쥴 생성 API", description = "1. 알림 생성 체크 시 프로젝트 내 멤버들에게 알림 생성\n2. time은 HH:MM:SS 식으로 보내주세요!")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "204", description = "성공", content = @Content(mediaType = "application/json")),
-            @ApiResponse(responseCode = "PROJECT-001", description = "프로젝트를 찾을 수 없습니다.", content = @Content(mediaType = "application/json")),
-            @ApiResponse(responseCode = "AUTH-001", description = "사용자를 찾을 수 없습니다.", content = @Content(mediaType = "application/json")),
-    })
     public ResponseEntity<Void> createSchedule(
             @AuthenticationPrincipal OAuth2UserPrincipal principal,
             @Parameter(required = true, description = "프로젝트 ID", in = ParameterIn.PATH)
@@ -66,9 +61,6 @@ public class ScheduleController {
 
     @GetMapping("/{projectID}")
     @Operation(summary = "프로젝트 캘린더 목록 API", description = "특정 프로젝트의 Meeting 및 Schedule 목록")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "성공", content = @Content(mediaType = "application/json")),
-    })
     public ResponseEntity<SchedulesAndMeetingsListResponseDTO> findAllSchedules(
             @Parameter(required = true, description = "프로젝트 ID", in = ParameterIn.PATH)
             @PathVariable("projectID") Long projectId) {
@@ -79,10 +71,6 @@ public class ScheduleController {
 
     @PostMapping("/{scheduleID}")
     @Operation(summary = "개인별 스케쥴 참여 API", description = "시간표 드래그 후 입력 시 요청, 30분 단위로 보내주세용")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "204", description = "성공", content = @Content(mediaType = "application/json")),
-            @ApiResponse(responseCode = "SCHEDULE-001", description = "일정을 찾을 수 없습니다.", content = @Content(mediaType = "application/json")),
-    })
     public ResponseEntity<Void> joinSchedule(
             @Parameter(required = true, description = "스케쥴 ID", in = ParameterIn.PATH)
             @PathVariable("scheduleID") Long scheduleId,
@@ -95,9 +83,6 @@ public class ScheduleController {
 
     @GetMapping("/{projectID}/detail/{scheduleID}")
     @Operation(summary = "스케쥴 상세 조회 API", description = "개인별 입력한 스케쥴 시간 List + 참여한 인원 List 조회")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "성공", content = @Content(mediaType = "application/json")),
-    })
     public ResponseEntity<List<ScheduleTimeWithMemberInfoDTO>> detailSchedule(
             @Parameter(required = true, description = "프로젝트 ID", in = ParameterIn.PATH)
             @PathVariable("projectID") Long projectId,
